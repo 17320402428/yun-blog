b7eea741f9419a7ed652c6f7922ba114ad698b2f
db <nm.dongbin.aisino.com>
2023-05-16 16:54:33 +0800

    用户管理

--- src/api/user/types/user.ts
+++ src/api/user/types/user.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: bin
+ * @Date: 2023-04-28 15:39:05
+ * @LastEditors: bin
+ * @LastEditTime: 2023-05-16 11:29:03
+ * @objectDescription: 入口文件
+ */
 export interface IGetTableData {
   username: string,
   id: string,
